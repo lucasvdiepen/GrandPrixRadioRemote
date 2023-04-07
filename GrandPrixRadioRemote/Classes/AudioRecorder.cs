45c86c67fc087c4e835e3ee437be8cbd2642de0a
lucasvdiepen <49072755+lucasvdiepen@users.noreply.github.com>
2023-04-07 22:36:53 +0200

    Fixed gc issue

--- GrandPrixRadioRemote/Classes/AudioRecorder.cs
+++ GrandPrixRadioRemote/Classes/AudioRecorder.cs
@@ -21,11 +21,9 @@ namespace GrandPrixRadioRemote.Classes
         private void RecordMicNAudio()
         {
             waveInEvent = new WasapiLoopbackCapture();
-            //waveInEvent.DeviceNumber = 0;
             waveInEvent.WaveFormat = new NAudio.Wave.WaveFormat(rate: SAMPLE_RATE, bits: 16, channels: 1);
             waveInEvent.DataAvailable += (o, e) => onDataAvailable.Invoke(e);
             waveInEvent.RecordingStopped += (o, e) => Console.WriteLine("Recording stopped.");
-            //waveInEvent.BufferMilliseconds = 1000;
             waveInEvent.StartRecording();
         }
 
@@ -39,6 +37,9 @@ namespace GrandPrixRadioRemote.Classes
         public void StopRecording()
         {
             if (recordingTask == null) return;
+
+            waveInEvent.StopRecording();
+            waveInEvent.Dispose();
 
             recordingTask.Dispose();
             recordingTask = null;
